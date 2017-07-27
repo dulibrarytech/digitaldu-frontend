4643a58c671979963d507bd6003130f2996485b0
Jeff <you@example.com>
2017-07-27 15:47:18 -0600

    Add collection tn image element dynamic loop

--- discovery/service.js
+++ discovery/service.js
@@ -3,19 +3,27 @@
 const es = require('elasticsearch'),
       config = require('../config/config');
 
-// var client = new es.Client({
-//     host: config.elasticsearchHost + ':' + config.elasticsearchPort
-// });
+var client = new es.Client({
+    host: config.elasticsearchHost + ':' + config.elasticsearchPort
+});
 
 exports.getCollections = function(pid) {
 	var collections = [];
 
 	// DEV test
 	var tn = config.fedoraPath + "/fedora/objects/codu:70104/datastreams/TN/content";
-	collections[0] = {
+	collections.push({
 		pid: "codu:70104",
 		tn: tn
-	}
+	});
+	tn = config.fedoraPath + "/fedora/objects/codu:59239/datastreams/TN/content";
+	collections.push({
+		pid: "codu:59239",
+		tn: tn
+	});
+
+	// Query ES for all objects with rels-ext/isMemberOfCollection == pid
+	// Build collection object, push to array
 
 	return collections;
 }
