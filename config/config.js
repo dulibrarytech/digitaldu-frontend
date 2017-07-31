598619d752b2db6b63f377339be47f314e4f0762
Jeff <you@example.com>
2017-07-31 11:49:59 -0600

    Add baseurl to settings

--- config/config.js
+++ config/config.js
@@ -1,12 +1,13 @@
 'use strict';
 
 module.exports = {
-    // Application settings
-    fedoraPath: "http://librepo01-vlp.du.edu:8080",
-    topLevelCollectionPID: "codu_root",
-
-    // .env configuration
+    // Appliation settings
+    baseUrl: process.env.APP_HOST + ":" + process.env.APP_PORT,
     elasticsearchHost: process.env.ELASTICSEARCH_HOST,
     elasticsearchPort: process.env.ELASTICSEARCH_PORT,
-    elasticsearchIndex: process.env.ELASTICSEARCH_INDEX
+    elasticsearchIndex: process.env.ELASTICSEARCH_INDEX,
+
+    // Repository settings
+    fedoraPath: "http://librepo01-vlp.du.edu:8080",
+    topLevelCollectionPID: "codu_root"
 };
