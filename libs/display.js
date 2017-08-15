c69d31e375dcc9c72a57363074cec2c99409cb5a
Jeff <you@example.com>
2017-08-15 16:19:24 -0600

    Remove display component and add display library

---
+++ libs/display.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const config = require('../config/config');
+
+var getSmallImageViewer = function() {
+
+};
+
+var getLargeImageViewer = function() {
+
+};
+
+var getPDFViewer = function() {
+
+};
+
+var getAudioObjectPlayer = function() {
+
+};
+
+var getVideoObjectPlayer = function() {
+
+};
+
+exports.getObjectViewer = function(data) {
+
+}
