35a7c27e15bd1c49bf3eaf225e3276580d822600
Jeff <you@example.com>
2017-08-28 14:52:00 -0600

    Dynamic facet search update url working

--- libs/facets.js
+++ libs/facets.js
@@ -24,7 +24,9 @@ function createList(facet, data) {
 
        // html += '<span><a  href="/repository/search?type=All&q=Men%27y#">' + data[i].key + '</a>&nbsp;&nbsp;(' + data[i].doc_count + ')</span><br>';
 
-        html += '<span><a  href="javascript:document.location.href=selectFacet(\'' + facet + '\', \'' + data[i].key + '\');">' + data[i].key + '</a>&nbsp;&nbsp;(' + data[i].doc_count + ')</span><br>';
+        html += '<span><a  href="javascript:document.location.href=selectFacet(\'' + facet + '\', \'' + data[i].key + '\');">' + data[i].key + '</a>&nbsp;&nbsp;(' + data[i].doc_count + ')</span><br>';   // good
+
+        //html += '<span><a  onclick="selectFacet(\'' + facet + '\', \'' + data[i].key + '\');">' + data[i].key + '</a>&nbsp;&nbsp;(' + data[i].doc_count + ')</span><br>';
     }
 
     return html;
