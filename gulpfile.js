64cb96aebbc8e53df11a61c1c6666b18418a86fd
Jeff <you@example.com>
2017-10-15 17:36:59 -0600

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,20 @@
 var gulp = require('gulp'),
-    uglify = require('gulp-uglify'),
+    uglify = require('gulp-uglify-es').default,
     plumber = require('gulp-plumber');
 
 gulp.task('default', function () {
+    return 0;
+});
+
+gulp.task('dependancies', function () {
     return gulp.src('./public/assets/js/*.js')
-        .pipe(plumber())
         .pipe(uglify())
-        .pipe(gulp.dest('./public/dist'));
+        .pipe(gulp.dest('./public/dist/assets/js/'));
+});
+
+gulp.task("libs", function () {
+    return gulp.src("./libs/*.js")
+        .pipe(uglify(/* options */))
+        // .pipe(rename("bundle.min.js"))
+        .pipe(gulp.dest("./public/dist/libs/"));
 });
