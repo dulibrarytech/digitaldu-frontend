aaed6f4313c97b57f76370a663ccc0b84721ddda
Jeff <you@example.com>
2018-06-05 11:16:07 -0600

    Update server connection message

--- digital-du.js
+++ digital-du.js
@@ -9,6 +9,6 @@ var app = express();
 
 app.listen(process.env.APP_PORT);
 
-console.log('digital-du application running at http://' + process.env.APP_HOST + ':' + process.env.APP_PORT + ' in ' + process.env.NODE_ENV + ' mode.');
+console.log('Digital-DU application running at http://' + process.env.APP_HOST + ':' + process.env.APP_PORT + ' in ' + process.env.NODE_ENV + ' mode.');
 
 module.exports = app;
