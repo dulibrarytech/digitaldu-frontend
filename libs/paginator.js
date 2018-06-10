71dd4d2f96c1de2558317b13b6aa8d97cbe5ed4b
Jeff <you@example.com>
2018-06-10 11:14:40 -0600

    Add functional paginator to views, add button function

--- libs/paginator.js
+++ libs/paginator.js
@@ -2,7 +2,7 @@
  * Creates the view object containing the paginator data
  */
 
-exports.create = function(items, page, pageCount) {
+exports.create = function(items, page, maxItems) {
 	var pagination = {
 		page: page,
 		beginCount: 0,
@@ -11,20 +11,42 @@ exports.create = function(items, page, pageCount) {
 	};
 
 	// First item on the current page
-	pagination.beginCount = (pageCount * (page-1)) + 1;
+	pagination.beginCount = (maxItems * (page-1)) + 1;
 
 	// Items on the current page
-	if(items.list.length < pageCount) {
+	if(items.length < maxItems) {
 
 		// This is the 'last page', when page count > number of items left to display.
-		pagination.pageHits = (pagination.beginCount - 1) + items.list.length;
+		pagination.pageHits = (pagination.beginCount - 1) + items.length;
 	}
 	else {	
-		pagination.pageHits = items.list.length * page;
+		pagination.pageHits = items.length * page;
 	}
 
 	// The total number of search results
-	pagination.totalHits = items.count;
-
+	pagination.totalHits = items.length;
+	pagination['buttons'] = getButtons(items.length, page, maxItems);
+		console.log("TEST buttons are", pagination.buttons);
 	return pagination;
+}
+
+var getButtons = function(pageItemCount, page, maxItems) {
+	var buttons = {
+		prev: 0,
+		next: 0
+	}
+	if(pageItemCount <= maxItems && maxItems % pageItemCount == 0) {
+		buttons.next = 0;
+		buttons.prev = page-1;
+	}
+	else if(pageItemCount == maxItems && page == 1) {
+		buttons.next = page+1;
+		buttons.prev = 0;
+	}
+	else {
+		buttons.next = page+1;
+		buttons.prev = page-1;
+	}
+
+	return buttons;
 }
