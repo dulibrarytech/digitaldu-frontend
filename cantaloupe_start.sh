5cc1004b8c68de9da13b5908c380eb5be7b61a85
Jeff <you@example.com>
2018-10-01 11:27:01 -0600

    Remove local cantaloupe script

--- cantaloupe_start.sh
+++ cantaloupe_start.sh
@@ -1,1 +1,1 @@
-java -Dcantaloupe.config=/usr/local/etc/cantaloupe/cantaloupe.properties -Xmx2g -jar /usr/local/etc/cantaloupe/Cantaloupe-3.3.4.war
+java -Dcantaloupe.config=/usr/local/etc/cantaloupe4/cantaloupe.properties -Xmx2g -jar /usr/local/etc/cantaloupe4/Cantaloupe-3.3.4.war
