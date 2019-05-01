8ff5b68b0679a42c31642b278feced7f110bb9be
Jeff Rynhart <jeff.rynhart@du.edu>
2019-05-01 15:20:35 -0600

    Add events.js

---
+++ public/assets/js/events.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+    
+	// Select an option in the results per page dropdown
+	$('#results-per-page').click(function(event) {
+		var searchUrl = decodeURIComponent(window.location.href).replace(/&resultsPerPage=[0-9]+/g, "");
+		searchUrl += "&resultsPerPage=" + $('#results-per-page').val();
+		window.location.replace(searchUrl);
+	});
+
+});
+
+
