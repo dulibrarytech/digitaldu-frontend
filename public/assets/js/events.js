305f0b21efaa44bd0c06250308ffdafd6a44c1ab
Jeff Rynhart <jeff.rynhart@du.edu>
2019-05-01 15:23:12 -0600

    Url encode the search url after adding the resultsPerPage parameter

--- public/assets/js/events.js
+++ public/assets/js/events.js
@@ -4,7 +4,7 @@ $( document ).ready(function() {
 	$('#results-per-page').click(function(event) {
 		var searchUrl = decodeURIComponent(window.location.href).replace(/&resultsPerPage=[0-9]+/g, "");
 		searchUrl += "&resultsPerPage=" + $('#results-per-page').val();
-		window.location.replace(searchUrl);
+		window.location.replace(encodeURI(searchUrl));
 	});
 
 });
