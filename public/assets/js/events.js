95068bc785dbbfb2c27632e04264152cd0ecaa48
Jeff Rynhart <jeff.rynhart@du.edu>
2019-05-14 12:56:28 -0600

    Update dropdown event type for results count and results display dropdown options

--- public/assets/js/events.js
+++ public/assets/js/events.js
@@ -1,14 +1,14 @@
 $( document ).ready(function() {
     
 	// Select an option in the results per page dropdown.  Reset page selection to 1 (no parameter), remove existing page count parameter
-	$('#results-per-page').click(function(event) {
+	$('#results-per-page').change(function(event) {
 		var searchUrl = decodeURIComponent(window.location.href).replace(/&resultsPerPage=[0-9]+/g, "");
 		searchUrl = searchUrl.replace(/&*page=[0-9]+/g, "");
 		searchUrl += "&resultsPerPage=" + $('#results-per-page').val();
 		window.location.replace(encodeURI(searchUrl));
 	});
 
-	$('#results-view-select').click(function(event) {
+	$('#results-view-select').change(function(event) {
 		var searchUrl = decodeURIComponent(window.location.href).replace(/&view=[a-zA-Z0-9]+/g, "");
 		searchUrl += "&view=" + $('#results-view-select').val();
 		window.location.replace(encodeURI(searchUrl));
