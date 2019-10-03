64f62ea1494b6b60eb1dd5b0a0b0ce778e1d4e96
Jeff Rynhart <jeff.rynhart@du.edu>
2019-10-03 14:51:01 -0600

    Move server startup log messages to app listen() callback function

--- discovery.js
+++ discovery.js
@@ -4,18 +4,16 @@
  * discovery.js
  * Discovery app node bootstrap file
  */
-
-'use strict';
-
+ 
 require('dotenv').load();
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
 var express = require('./config/express');
 var app = express();
 
-app.listen(process.env.APP_PORT);
-
-console.log("Digital-DU application running on port " + process.env.APP_PORT + " in " + process.env.NODE_ENV + " mode.");
-console.log("Repository: " + process.env.REPOSITORY_DOMAIN);
+app.listen(process.env.APP_PORT, () => {
+	console.log("Digital-DU application running on port " + process.env.APP_PORT + " in " + process.env.NODE_ENV + " mode.");
+	console.log("Repository: " + process.env.REPOSITORY_DOMAIN);
+});
 module.exports = app;
