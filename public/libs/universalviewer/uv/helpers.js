213dc53a5efc5276393e27858f322c621e724401
Jeff Rynhart <jeff.rynhart@du.edu>
2020-03-18 20:16:38 -0600

    Increase timeout before appending loading message

--- public/libs/universalviewer/uv/helpers.js
+++ public/libs/universalviewer/uv/helpers.js
@@ -33,7 +33,7 @@ function createUV(selector, data, dataProvider) {
     uv.on('create', function(obj) {
         setTimeout(function(){ 
             $(".spinner").append('<div class="loading-msg">Loading, please wait...</div>');
-        }, 500);
+        }, 1000);
         setTimeout(function(){  
             $(".loading-msg").css("display", "none");
             $(".spinner").append("<div class='timeout-msg'><h6>We're sorry, this is taking longer than expected. To report any problems with accessing this resource, please contact <a href='mailto:archives.du.edu'>archives.du.edu</a></h6></div>")
