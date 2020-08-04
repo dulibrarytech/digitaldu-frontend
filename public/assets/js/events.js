9fc07c6c4b8e567aa1a557847cb45e6fa00f15b6
Jeff Rynhart <jeff.rynhart@du.edu>
2020-08-04 17:22:55 -0600

    Add goto page form to paginator #335

--- public/assets/js/events.js
+++ public/assets/js/events.js
@@ -22,6 +22,24 @@ $( document ).ready(function() {
 		window.location.replace(encodeURI(searchUrl));
 	});
 
+	$('#set-page-number-button').click(function(event) {
+		if($('#set-page-number').val().match(/^[0-9]+$/)) {
+			var searchUrl = decodeURIComponent(window.location.href).replace(/[&?]page=[a-zA-Z0-9, ]+/g, ""),
+				val = $('#set-page-number').val() <= parseInt($('#page-count').html()) ? $('#set-page-number').val() : parseInt($('#page-count').html());
+
+			if(searchUrl.indexOf("?") >= 0) {
+				searchUrl += ("&page=" + val)
+			}
+			else {
+				searchUrl += ("?page=" + val)
+			}
+			window.location.replace(encodeURI(searchUrl));
+		}
+		else {
+			console.log("Invalid input, must be integer")
+		}
+	});
+
 	$("#add-query-button").click(function(event) {
 		$("#add-query-button").blur();
 	});
