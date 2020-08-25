fbba477e821c4b7978aec72a98efc1ba0d16f31e
Jeff Rynhart <jeff.rynhart@du.edu>
2020-08-25 16:39:17 -0600

    Removed overview page link from footer, removed unused static page routes #337

--- specialcollections/routes.js
+++ specialcollections/routes.js
@@ -28,39 +28,14 @@ var data = {
 };
 
 module.exports = function (app) {
-    app.route('/about/overview')
-    .get(function(req, res) {
-    	return res.render('static/overview', data)
-    });
-
     app.route('/about/contact')
     .get(function(req, res) {
     	return res.render('static/contact', data)
     });
 
-    app.route('/about/faqs')
-    .get(function(req, res) {
-    	return res.render('static/faqs', data)
-    })
-
     app.route('/content/copyright-information')
     .get(function(req, res) {
     	return res.render('static/copyright-information', data)
-    });
-
-    app.route('/services/consultation-and-training')
-    .get(function(req, res) {
-    	return res.render('static/consultation-and-training', data)
-    });
-
-    app.route('/services/digital-content-management')
-    .get(function(req, res) {
-    	return res.render('static/digital-content-management', data)
-    });
-
-    app.route('/help/glossary')
-    .get(function(req, res) {
-    	return res.render('static/glossary', data)
     });
 };
 
