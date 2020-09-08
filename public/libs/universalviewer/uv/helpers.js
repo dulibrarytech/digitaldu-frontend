2f1a83433ef54f79c661f8ff84ee7f3c1ffff591
Jeff Rynhart <jeff.rynhart@du.edu>
2020-09-08 10:23:32 -0600

    Updated the error contact email address to 'archives@du.edu'

--- public/libs/universalviewer/uv/helpers.js
+++ public/libs/universalviewer/uv/helpers.js
@@ -32,7 +32,7 @@ function createUV(selector, data, dataProvider) {
     uv.on('create', function(obj) {
         setTimeout(function(){ 
             $(".spinner").append('<div class="loading-msg">Loading, please wait...</div>');
-            $(".spinner").append("<div class='timeout-msg' style='display: none'><h6>We're sorry, this is taking longer than expected. To report any problems with accessing this resource, please contact <a href='mailto:archives.du.edu'>archives.du.edu</a></h6></div>")
+            $(".spinner").append("<div class='timeout-msg' style='display: none'><h6>We're sorry, this is taking longer than expected. To report any problems with accessing this resource, please contact <a href='mailto:archives@du.edu'>archives@du.edu</a></h6></div>")
         }, 1000);
         setTimeout(function(){  
             $(".loading-msg").css("display", "none");
