bcc919367fe7bb222f5d8b0f386350d3b21ff5fc
Jeff Rynhart <jeff.rynhart@du.edu>
2020-11-03 11:00:07 -0700

    Added client view modules to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -22,4 +22,24 @@ var deploy_js = function() {
 }
 exports.deploy_js = deploy_js;
 
-gulp.task('deploy_all', gulp.series(obfuscate_js, deploy_js))
+var minify_view_js = function() {
+  return gulp.src('./public/views/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./public/dist/views/'));
+}
+exports.minify_view_js = minify_js;
+
+var obfuscate_view_js = function() {
+  return gulp.src('./public/views/*.js')
+    .pipe(javascriptObfuscator())
+    .pipe(gulp.dest('./public/dist/views/'));
+}
+exports.obfuscate_view_js = obfuscate_js;
+
+var deploy_view_js = function() {
+  return gulp.src('./public/dist/views/*.js')
+    .pipe(gulp.dest('./public/views/'));
+}
+exports.deploy_view_js = deploy_js;
+
+gulp.task('deploy_all', gulp.series(obfuscate_js, deploy_js, obfuscate_view_js, deploy_view_js))
