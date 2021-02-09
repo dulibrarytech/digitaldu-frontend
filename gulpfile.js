077faa6b35c46197c30f66bb5d8d95be6af222b2
Jeff Rynhart <jeff.rynhart@du.edu>
2021-02-09 09:48:35 -0700

    Update gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -27,19 +27,19 @@ var minify_view_js = function() {
     .pipe(uglify())
     .pipe(gulp.dest('./public/dist/views/'));
 }
-exports.minify_view_js = minify_js;
+exports.minify_view_js = minify_view_js;
 
 var obfuscate_view_js = function() {
   return gulp.src('./public/views/*.js')
     .pipe(javascriptObfuscator())
     .pipe(gulp.dest('./public/dist/views/'));
 }
-exports.obfuscate_view_js = obfuscate_js;
+exports.obfuscate_view_js = obfuscate_view_js;
 
 var deploy_view_js = function() {
   return gulp.src('./public/dist/views/*.js')
     .pipe(gulp.dest('./public/views/'));
 }
-exports.deploy_view_js = deploy_js;
+exports.deploy_view_js = deploy_view_js;
 
 gulp.task('deploy_all', gulp.series(obfuscate_js, deploy_js, obfuscate_view_js, deploy_view_js))
