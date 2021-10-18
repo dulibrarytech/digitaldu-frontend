d08de0002ba27b9a9cacb13bc83fd482ae9440f2
Jeff Rynhart <Jeff.Rynhart@du.edu>
2021-10-17 18:14:16 -0600

    Added get request method to http-request lib #388

--- libs/http-request.js
+++ libs/http-request.js
@@ -41,6 +41,27 @@ exports.head = async function(url, callback) {
     }); 
 }
 
+exports.get = function(url, data, callback) {
+    axios.get(url, data).then(function(response) {
+        callback(null, response.status, response.data, response.headers)
+    }).catch(function(error) {
+        if(error.response) {
+            console.log("Url:", url);
+            console.log("Response status text:", error.response.statusText);
+
+            if(error.response.status == 500) {
+                callback(error.response.statusText, 500, null);
+            }
+            else {
+                callback(null, error.response.status, null, error.response.headers)
+            }
+        }
+        else {
+            callback(error, 500, null)
+        }
+    }); 
+}
+
 exports.get_stream = function(url, data, callback) {
     if(Object.keys(data).length !== 0) {
         let qstring = "?", count = 0;
