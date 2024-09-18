2d957ea195dd9c9e5e14bb8ec6a939f3c139c335
Jeff Rynhart <jeff.rynhart@du.edu>
2024-09-18 14:08:01 -0600

    added app baseurl to CSP img-src list in express

--- config/express.js
+++ config/express.js
@@ -21,89 +21,90 @@
  * Discovery app express.js bootstrap file
  */
 
-'use strict';
+ 'use strict';
 
-const config = require('../config/' + process.env.CONFIGURATION_FILE),
-    http = require('http'),
-    express = require('express'),
-    compression = require('compression'),
-    helmet = require('helmet'),
-    bodyParser = require('body-parser'),
-    noCache = require('nocache'),
-    cors = require('cors');
-
-module.exports = function () {
-    var app = express(),
-        server = http.createServer(app);
-
-    if (process.env.NODE_ENV === 'development') {
-        process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
-    } else if (process.env.NODE_ENV === 'production') {
-        app.use(compression());
-    }
-
-    app.use(cors({
-        origin: true
-    }));
-
-    app.use(bodyParser.urlencoded({
-        extended: true
-    }));
-    app.use(bodyParser.json());
-    app.use(noCache());
-    
-    app.use(
-        helmet({
-          contentSecurityPolicy: {
-            directives: {
-                "default-src": [
-                    "'self'", 
-                    config.webSocketDomain, 
-                    (config.webSocketDomain+":"+config.webSocketPort), 
-                    config.IIIFServerDomain, 
-                    config.IIIFDomain, 
-                    config.repositoryDomain, 
-                    'www.google-analytics.com', 
-                    'cdnapisec.kaltura.com', 
-                    'data:', 
-                    'blob:', 
-                    'www.du.edu', 
-                    'fonts.gstatic.com', 
-                    'use.fontawesome.com', 
-                    'http://jwpltx.com'],
-        
-                "style-src": ["'self'", "'unsafe-inline'", 'maxcdn.bootstrapcdn.com', 'use.fontawesome.com', 'vjs.zencdn.net', 'code.jquery.com', 'fonts.googleapis.com'],
-                "script-src": ["'self'", "'unsafe-inline'", "'unsafe-eval'", 'www.google-analytics.com', 'vjs.zencdn.net', 'use.fontawesome.com', 'code.jquery.com', 'http://p.jwpcdn.com', 'www.googletagmanager.com'],
-                "script-src-attr": ["'self'", "'unsafe-inline'"],
-                "font-src": ["'self'", 'data:', 'fonts.gstatic.com', 'use.fontawesome.com'],
-                "img-src": ["'self'", 'data:', "specialcollections.du.edu", "www.du.edu"],
-                "frame-ancestors": null,
-                "form-action": null
-            }
-          },
-
-          strictTransportSecurity: {
-              maxAge: 31536000
-          },
-
-          xFrameOptions: false
-        })
-    );
-
-    app.disable('x-powered-by');
-    app.use(express.static('./public'));
-    app.set('views', './views');
-    app.set('view engine', 'ejs');
-
-    require('../discovery/routes.js')(app);
-    require('../search/routes.js')(app);
-    require('../specialcollections/routes.js')(app);
-    require('express-template-cache');
-
-    app.route('/')
-        .get(function(req, res) {
-            res.redirect(config.rootUrl);
-    });
-
-    return server;
-};
+ const config = require('../config/' + process.env.CONFIGURATION_FILE),
+     http = require('http'),
+     express = require('express'),
+     compression = require('compression'),
+     helmet = require('helmet'),
+     bodyParser = require('body-parser'),
+     noCache = require('nocache'),
+     cors = require('cors');
+ 
+ module.exports = function () {
+     var app = express(),
+         server = http.createServer(app);
+ 
+     if (process.env.NODE_ENV === 'development') {
+         process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+     } else if (process.env.NODE_ENV === 'production') {
+         app.use(compression());
+     }
+ 
+     app.use(cors({
+         origin: true
+     }));
+ 
+     app.use(bodyParser.urlencoded({
+         extended: true
+     }));
+     app.use(bodyParser.json());
+     app.use(noCache());
+     
+     app.use(
+         helmet({
+           contentSecurityPolicy: {
+             directives: {
+                 "default-src": [
+                     "'self'", 
+                     config.webSocketDomain, 
+                     (config.webSocketDomain+":"+config.webSocketPort), 
+                     config.IIIFServerDomain, 
+                     config.IIIFDomain, 
+                     config.repositoryDomain, 
+                     'www.google-analytics.com', 
+                     'cdnapisec.kaltura.com', 
+                     'data:', 
+                     'blob:', 
+                     'www.du.edu', 
+                     'fonts.gstatic.com', 
+                     'use.fontawesome.com', 
+                     'http://jwpltx.com'],
+         
+                 "style-src": ["'self'", "'unsafe-inline'", 'maxcdn.bootstrapcdn.com', 'use.fontawesome.com', 'vjs.zencdn.net', 'code.jquery.com', 'fonts.googleapis.com'],
+                 "script-src": ["'self'", "'unsafe-inline'", "'unsafe-eval'", 'www.google-analytics.com', 'vjs.zencdn.net', 'use.fontawesome.com', 'code.jquery.com', 'http://p.jwpcdn.com', 'www.googletagmanager.com'],
+                 "script-src-attr": ["'self'", "'unsafe-inline'"],
+                 "font-src": ["'self'", 'data:', 'fonts.gstatic.com', 'use.fontawesome.com'],
+                 "img-src": ["'self'", config.baseUrl, 'data:', "specialcollections.du.edu", "www.du.edu"],
+                 "frame-ancestors": null,
+                 "form-action": null
+             }
+           },
+ 
+           strictTransportSecurity: {
+               maxAge: 31536000
+           },
+ 
+           xFrameOptions: false
+         })
+     );
+ 
+     app.disable('x-powered-by');
+     app.use(express.static('./public'));
+     app.set('views', './views');
+     app.set('view engine', 'ejs');
+ 
+     require('../discovery/routes.js')(app);
+     require('../search/routes.js')(app);
+     require('../specialcollections/routes.js')(app);
+     require('express-template-cache');
+ 
+     app.route('/')
+         .get(function(req, res) {
+             res.redirect(config.rootUrl);
+     });
+ 
+     return server;
+ };
+ 
