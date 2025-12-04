341ab34e5bd8609fb541302c7b642e682747c907
Jeff Rynhart <jeff.rynhart@du.edu>
2025-12-04 10:15:45 -0700

    updated universalviewer image load error message text
    github #429

--- universalviewer-files/uv_helpers.js
+++ universalviewer-files/uv_helpers.js
@@ -77,7 +77,7 @@ function createUV(selector, data, dataProvider) {
         const canvasIndex = dataProvider.get('cv') || 0;
 
         if (elementIndex == canvasIndex) {
-            const message = "We're sorry, this image could not be loaded.<br>To report the problem, please contact <a href='mailto:archives@du.edu'>archives@du.edu</a>";
+            const message = "We're sorry, there was a problem loading the image.<br>To report the problem, please contact <a href='mailto:archives@du.edu'>archives@du.edu</a>";
             showMessageBox($(".mainPanel .content"), message);
             hasError = true;
         }
